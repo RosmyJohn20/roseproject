7f5e9e51ecdd95be886420587662b204bc931573
RosmyJohn20 <rosmyjohnthokkudam@gmail.com>
2018-03-06 14:53:40 +0530

    rosmy commited her first program

---
+++ sum.py
@@ -0,0 +1,15 @@
+a=input('enter a value')
+b=input('enter a value')
+c=int(a)+int(b)
+d=int(a)-int(b)
+e=int(a)*int(b)
+f=int(a)/int(b)
+g=int(a)**int(b)
+
+print  (c)
+print  (d)
+print  (e)
+print  (f)
+print  (g)
+
+
